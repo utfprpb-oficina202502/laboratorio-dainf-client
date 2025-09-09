34c8f719b0fe10361a6c1bdac1fe9697afad0790
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-09-09 19:10:25 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //api_url: 'http://localhost:8090/server/'
-  api_url: 'https://dainf-labs-api.app.pb.utfpr.edu.br/',
+  api_url: 'https://dainf-server-dev-e1c111fedbcc.herokuapp.com/',
   google_client_id: '726155933363-am0vqngulv5soqerhhprndt5kj4judtl.apps.googleusercontent.com',
   minio_url: 'https://minio.app.pb.utfpr.edu.br/dainf-labs/'
   //api_url: 'https://tcc-server-utfpr.herokuapp.com/server/'
