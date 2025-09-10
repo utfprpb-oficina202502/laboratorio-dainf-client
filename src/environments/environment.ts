a2464815bcae5e684bd5dfa9d9d4bf4ea23f963c
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-09-10 18:22:48 -0300

    remoção de valores extras

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-import { defaultChartColors } from '../app/framework/charts/chart-colors.config';
+import {defaultChartColors} from '../app/framework/charts/chart-colors.config';
 
 export const environment = {
   production: false,
-  //api_url: 'https://tcc-server-utfpr.herokuapp.com/server/'
-  //api_url: 'http://172.20.190.22:8099/server/'
-  // api_url: 'http://localhost:8099/server/',
   api_url: 'https://test-labs-api.app.pb.utfpr.edu.br/',
   google_client_id: '726155933363-am0vqngulv5soqerhhprndt5kj4judtl.apps.googleusercontent.com',
   minio_url: 'http://localhost:9000/dainf-labs/',
@@ -17,11 +10,3 @@ export const environment = {
   }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
