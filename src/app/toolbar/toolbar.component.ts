05560173e645f7e01ece6d417aa0b8727a88d006
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-09-29 16:06:32 -0300

    SCRUM-46 - Migração de material para PrimeNG

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -2,9 +2,7 @@ import { Component, HostListener, OnInit } from "@angular/core";
 import { LoginService } from "../login/login.service";
 import { SidenavService } from "../sidenav/sidenav.service";
 import { MenuItem } from "primeng/api";
-// import { SocialAuthService } from "@abacritt/angularx-social-login";
 import { Router } from "@angular/router";
-import { Usuario } from "../usuario/usuario";
 
 @Component({
     selector: "app-toolbar",
@@ -16,24 +14,20 @@ export class ToolbarComponent implements OnInit {
   widthScreen: number;
   sidenavIsOpen: boolean;
   items: MenuItem[];
-  fotoUrl: string;
 
   constructor(
-    private loginService: LoginService,
-    private sidenavService: SidenavService,
-    // private socialAuthService: SocialAuthService,
-    private router: Router
+    private readonly loginService: LoginService,
+    private readonly sidenavService: SidenavService,
+    private readonly router: Router
   ) {}
 
   ngOnInit() {
     this.getScreenSize();
     this.buildListenerCloseDrawer();
     this.optionDropdown();
-    this.fotoUrl = JSON.parse(localStorage.getItem("userLogged")).fotoUrl;
   }
 
   logout() {
-    // this.socialAuthService.signOut();
     this.loginService.logout();
   }
 
