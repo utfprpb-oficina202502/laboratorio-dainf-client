9ba7ef3460e3b53dda72c7a91d5ea5cc44e53480
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-09-30 11:01:35 -0300

    chore - update na configuração do typescript e tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "!./src/**/*.spec.ts",  // Exclude tests
   ],
   theme: {
     extend: {
       screens: {
         'xs': '426px',
-        'mdp': '769px'
+        'mdp': '769px',
+      },
+      colors: {
+        'primary': 'var(--color-primary)',
+        'secondary': 'var(--color-secondary)',
       },
     },
-  },
+    plugins: [],
+  }
 }
