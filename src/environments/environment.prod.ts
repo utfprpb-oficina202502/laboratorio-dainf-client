fbfae319d732a73ac5b09c2f1650af88e1e9dfe9
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-10-05 13:40:42 -0300

    bugfix - url de prod para post_build do heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {defaultChartColors} from '../app/framework/charts/chart-colors.config';
 
 export const environment = {
   production: true,
-  api_url: 'https://dainf-server-dev-e1c111fedbcc.herokuapp.com/',
+  api_url: 'http://localhost:8080/',
   google_client_id: '726155933363-am0vqngulv5soqerhhprndt5kj4judtl.apps.googleusercontent.com',
   minio_url: 'https://minio.app.pb.utfpr.edu.br/dainf-labs/',
   charts: {
