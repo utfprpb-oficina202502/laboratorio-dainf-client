857d19e1ccc63c76fadebd2880fdaaaf5f264d86
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-11-29 19:18:54 -0300

    chore - mudança de endpoint do min.io para staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,8 @@ export const environment = {
   production: true,
   api_url: 'http://localhost:8080/',
   google_client_id: '726155933363-am0vqngulv5soqerhhprndt5kj4judtl.apps.googleusercontent.com',
-  minio_url: 'https://minio.app.pb.utfpr.edu.br/dainf-labs/',
+  //minio_url: 'https://minio.app.pb.utfpr.edu.br/dainf-labs/',
+  minio_url: 'https://kirinus.tec.br:9000/utfpr-bucket/',
   charts: {
     colors: defaultChartColors
   }
